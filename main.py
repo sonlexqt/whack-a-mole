dd5c3e1a4517626e1cc9911d45d6853f818b35f3
thaisonle94 <thaisonle94@gmail.com>
2015-09-09 03:55:27 +0700

    Display user score; restructure the code following OOP pattern

--- main.py
+++ main.py
@@ -4,9 +4,92 @@ from pygame import *
 
 
 # Define constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 FPS = 60
 MOLE_WIDTH = 90
 MOLE_HEIGHT = 81
+FONT_SIZE = 50
+GAME_TITLE = "Whack A Mole - Game Programming - Assignment 1"
+
+
+class GameManager:
+    def __init__(self):
+        self.score = 0
+        # Initialize screen
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption(GAME_TITLE)
+        self.background = pygame.image.load("images/bg.png")
+        # Font object for displaying text
+        self.font_obj = pygame.font.SysFont(None, FONT_SIZE)
+        # Initialize the mole's sprite sheet
+        sprite_sheet = pygame.image.load("images/mole.png")
+        self.mole = []
+        self.mole.append(sprite_sheet.subsurface(169, 0, 90, 81))
+        self.mole.append(sprite_sheet.subsurface(309, 0, 90, 81))
+        self.mole.append(sprite_sheet.subsurface(449, 0, 90, 81))
+        # Init debugger
+        self.debugger = Debugger("debug")
+
+    def update_score(self):
+        current_score_string = "SCORE: " + str(self.score)
+        text = self.font_obj.render(current_score_string, True, (255, 255, 255))
+        text_pos = text.get_rect()
+        text_pos.centerx = self.background.get_rect().centerx
+        text_pos.centery = 20
+        self.screen.blit(text, text_pos)
+
+    def start(self):
+        cycle_time = 0
+        num = -1
+        loop = True
+        is_down = False
+        interval = 0.1
+        frame_num = 0
+        # Time control variables
+        clock = pygame.time.Clock()
+
+        for i in range(len(self.mole)):
+            self.mole[i].set_colorkey((0, 0, 0))
+            self.mole[i] = self.mole[i].convert_alpha()
+
+        while loop:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    loop = False
+                if event.type == MOUSEBUTTONDOWN:
+                    if is_mole_hit(mouse.get_pos(), hole_positions[frame_num]):
+                        self.score += 1
+                        self.update_score()
+
+            if num == -1:
+                self.screen.blit(self.background, (0, 0))
+                self.update_score()
+                num = 0
+                is_down = False
+                interval = 0.5
+                frame_num = random.randint(0, 8)
+
+            mil = clock.tick(FPS)
+            sec = mil / 1000.0
+            cycle_time += sec
+            if cycle_time > interval:
+                pic = self.mole[num]
+                self.screen.blit(self.background, (0, 0))
+                self.screen.blit(pic, hole_positions[frame_num])
+                self.update_score()
+                if is_down is False:
+                    num += 1
+                else:
+                    num -= 1
+                if num > 2:
+                    num -= 1
+                    is_down = True
+                    interval = 0.3
+                else:
+                    interval = 0.1
+                cycle_time = 0
+            pygame.display.flip()
 
 
 class Debugger:
@@ -15,7 +98,7 @@ class Debugger:
 
     def log(self, message):
         if self.mode is "debug":
-            print("> DEBUG: " + message)
+            print("> DEBUG: " + str(message))
 
 
 def is_mole_hit(mouse_position, current_hole_position):
@@ -28,84 +111,24 @@ def is_mole_hit(mouse_position, current_hole_position):
     else:
         return False
 
-
-def game():
-    width, height = 800, 600
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Whack A Mole - Game Programming - Assignment 1')
-    background = pygame.image.load('images/bg.png')
-    screen.blit(background, (0, 0))
-    cycle_time = 0
-    num = -1
-    loop = True
-    down = False
-    interval = 0.1
-    frame_num = 0
-    
-    for i in range(len(mole)):
-        mole[i].set_colorkey((0, 0, 0))
-        mole[i] = mole[i].convert_alpha()
-
-    while loop:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = False
-            if event.type == MOUSEBUTTONDOWN:
-                if is_mole_hit(mouse.get_pos(), pos[frame_num]):
-                    debugger.log("Hit the mole !")
-
-        if num == -1:
-            screen.blit(background, (0, 0))
-            num = 0
-            down = False
-            interval = 0.5
-            frame_num = random.randint(0, 8)
-            
-        mil = clock.tick(FPS)
-        sec = mil / 1000.0
-        cycle_time += sec
-        if cycle_time > interval:
-            pic = mole[num]
-            screen.blit(background, (0, 0))
-            screen.blit(pic, pos[frame_num])
-            if down is False:
-                num += 1
-            else:
-                num -= 1
-            if num > 2:
-                num -= 1
-                down = True
-                interval = 0.3
-            else:
-                interval = 0.1
-            cycle_time = 0
-        pygame.display.flip()
-
+###############################################################
 # Initialize the game
 pygame.init()
-clock = pygame.time.Clock()
-delay_time = 0.2
-sprite_sheet = pygame.image.load("images/mole.png")
-mole = []
-mole.append(sprite_sheet.subsurface(169, 0, 90, 81))
-mole.append(sprite_sheet.subsurface(309, 0, 90, 81))
-mole.append(sprite_sheet.subsurface(449, 0, 90, 81))
-# Init debugger
-debugger = Debugger("debug")
 
 # Positions of the holes in background
-pos = []
-pos.append((381, 295))
-pos.append((119, 366))
-pos.append((179, 169))
-pos.append((404, 479))
-pos.append((636, 366))
-pos.append((658, 232))
-pos.append((464, 119))
-pos.append((95, 43))
-pos.append((603, 11))
+hole_positions = []
+hole_positions.append((381, 295))
+hole_positions.append((119, 366))
+hole_positions.append((179, 169))
+hole_positions.append((404, 479))
+hole_positions.append((636, 366))
+hole_positions.append((658, 232))
+hole_positions.append((464, 119))
+hole_positions.append((95, 43))
+hole_positions.append((603, 11))
 
 # Run the main loop
-game()
+my_game = GameManager()
+my_game.start()
 # Exit the game if the main loop ends
 pygame.quit()
