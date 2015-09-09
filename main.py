7e4f08a856144792389ca871e1d6674f085f6848
thaisonle94 <thaisonle94@gmail.com>
2015-09-09 21:19:27 +0700

    Increase difficulty by decreasing interval by player's level. Move all the game's code to the GameManager class.

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import random
 from pygame import *
 
 
-# Define constants
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-FPS = 60
-MOLE_WIDTH = 90
-MOLE_HEIGHT = 81
-FONT_SIZE = 50
-LEVEL_SCORE_GAP = 5
-GAME_TITLE = "Whack A Mole - Game Programming - Assignment 1"
-
-
 class GameManager:
     def __init__(self):
+        # Define constants
+        self.SCREEN_WIDTH = 800
+        self.SCREEN_HEIGHT = 600
+        self.FPS = 60
+        self.MOLE_WIDTH = 90
+        self.MOLE_HEIGHT = 81
+        self.FONT_SIZE = 50
+        self.LEVEL_SCORE_GAP = 4
+        self.GAME_TITLE = "Whack A Mole - Game Programming - Assignment 1"
+        # Initialize player's score, number of missed hits and level
         self.score = 0
         self.misses = 0
         self.level = 1
         # Initialize screen
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption(GAME_TITLE)
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.display.set_caption(self.GAME_TITLE)
         self.background = pygame.image.load("images/bg.png")
         # Font object for displaying text
-        self.font_obj = pygame.font.SysFont(None, FONT_SIZE)
+        self.font_obj = pygame.font.SysFont(None, self.FONT_SIZE)
         # Initialize the mole's sprite sheet
+        # 6 different states
         sprite_sheet = pygame.image.load("images/mole.png")
         self.mole = []
         self.mole.append(sprite_sheet.subsurface(169, 0, 90, 81))
@@ -34,12 +34,45 @@ class GameManager:
         self.mole.append(sprite_sheet.subsurface(575, 0, 116, 81))
         self.mole.append(sprite_sheet.subsurface(717, 0, 116, 81))
         self.mole.append(sprite_sheet.subsurface(853, 0, 116, 81))
+        # Positions of the holes in background
+        self.hole_positions = []
+        self.hole_positions.append((381, 295))
+        self.hole_positions.append((119, 366))
+        self.hole_positions.append((179, 169))
+        self.hole_positions.append((404, 479))
+        self.hole_positions.append((636, 366))
+        self.hole_positions.append((658, 232))
+        self.hole_positions.append((464, 119))
+        self.hole_positions.append((95, 43))
+        self.hole_positions.append((603, 11))
         # Init debugger
         self.debugger = Debugger("debug")
 
+    # Calculate the player level according to his current score & the LEVEL_SCORE_GAP constant
     def get_player_level(self):
-        return 1 + int(self.score / LEVEL_SCORE_GAP)
+        return 1 + int(self.score / self.LEVEL_SCORE_GAP)
 
+    # Get the new duration between the time the mole pop up and down the holes
+    # It's in inverse ratio to the player's current level
+    def get_interval_by_level(self, initial_interval):
+        new_interval = initial_interval - self.level * 0.15
+        if new_interval > 0:
+            return new_interval
+        else:
+            return 0.05
+
+    # Check whether the mouse click hit the mole or not
+    def is_mole_hit(self, mouse_position, current_hole_position):
+        mouse_x = mouse_position[0]
+        mouse_y = mouse_position[1]
+        current_hole_x = current_hole_position[0]
+        current_hole_y = current_hole_position[1]
+        if (mouse_x > current_hole_x) and (mouse_x < current_hole_x + self.MOLE_WIDTH) and (mouse_y > current_hole_y) and (mouse_y < current_hole_y + self.MOLE_HEIGHT):
+            return True
+        else:
+            return False
+
+    # Update the game states, re-calculate the player's score, misses, level
     def update(self):
         # Update the player's score
         current_score_string = "SCORE: " + str(self.score)
@@ -52,23 +85,26 @@ class GameManager:
         current_misses_string = "MISSES: " + str(self.misses)
         misses_text = self.font_obj.render(current_misses_string, True, (255, 255, 255))
         misses_text_pos = misses_text.get_rect()
-        misses_text_pos.centerx = SCREEN_WIDTH / 5 * 4
+        misses_text_pos.centerx = self.SCREEN_WIDTH / 5 * 4
         misses_text_pos.centery = 20
         self.screen.blit(misses_text, misses_text_pos)
         # Update the player's level
         current_level_string = "LEVEL: " + str(self.level)
         level_text = self.font_obj.render(current_level_string, True, (255, 255, 255))
         level_text_pos = level_text.get_rect()
-        level_text_pos.centerx = SCREEN_WIDTH / 5 * 1
+        level_text_pos.centerx = self.SCREEN_WIDTH / 5 * 1
         level_text_pos.centery = 20
         self.screen.blit(level_text, level_text_pos)
 
+    # Start the game's main loop
+    # Contains some logic for handling animations, mole hit events, etc..
     def start(self):
         cycle_time = 0
         num = -1
         loop = True
         is_down = False
         interval = 0.1
+        initial_interval = 1
         frame_num = 0
         left = 0
         # Time control variables
@@ -83,7 +119,7 @@ class GameManager:
                 if event.type == pygame.QUIT:
                     loop = False
                 if event.type == MOUSEBUTTONDOWN:
-                    if is_mole_hit(mouse.get_pos(), hole_positions[frame_num]) and num > 0 and left == 0:
+                    if self.is_mole_hit(mouse.get_pos(), self.hole_positions[frame_num]) and num > 0 and left == 0:
                         num = 3
                         left = 14
                         is_down = False
@@ -109,13 +145,13 @@ class GameManager:
                 interval = 0.5
                 frame_num = random.randint(0, 8)
 
-            mil = clock.tick(FPS)
+            mil = clock.tick(self.FPS)
             sec = mil / 1000.0
             cycle_time += sec
             if cycle_time > interval:
                 pic = self.mole[num]
                 self.screen.blit(self.background, (0, 0))
-                self.screen.blit(pic, (hole_positions[frame_num][0] - left, hole_positions[frame_num][1]))
+                self.screen.blit(pic, (self.hole_positions[frame_num][0] - left, self.hole_positions[frame_num][1]))
                 self.update()
                 if is_down is False:
                     num += 1
@@ -126,13 +162,15 @@ class GameManager:
                 elif num == 3:
                     num -= 1
                     is_down = True
-                    interval = 0.8 #TODO
+                    interval = self.get_interval_by_level(initial_interval)  # get the newly decreased interval value
                 else:
                     interval = 0.1
                 cycle_time = 0
+            # Update the display
             pygame.display.flip()
 
 
+# The Debugger class - use this class for printing out debugging information
 class Debugger:
     def __init__(self, mode):
         self.mode = mode
@@ -142,31 +180,9 @@ class Debugger:
             print("> DEBUG: " + str(message))
 
 
-def is_mole_hit(mouse_position, current_hole_position):
-    mouse_x = mouse_position[0]
-    mouse_y = mouse_position[1]
-    current_hole_x = current_hole_position[0]
-    current_hole_y = current_hole_position[1]
-    if (mouse_x > current_hole_x) and (mouse_x < current_hole_x + MOLE_WIDTH) and (mouse_y > current_hole_y) and (mouse_y < current_hole_y + MOLE_HEIGHT):
-        return True
-    else:
-        return False
-
 ###############################################################
 # Initialize the game
 pygame.init()
-
-# Positions of the holes in background
-hole_positions = []
-hole_positions.append((381, 295))
-hole_positions.append((119, 366))
-hole_positions.append((179, 169))
-hole_positions.append((404, 479))
-hole_positions.append((636, 366))
-hole_positions.append((658, 232))
-hole_positions.append((464, 119))
-hole_positions.append((95, 43))
-hole_positions.append((603, 11))
 
 # Run the main loop
 my_game = GameManager()
